d42933a31b4dc1ad0d9c36769ef332c670e1b7b4
Amer27 <34425111+Amer27@users.noreply.github.com>
2025-11-26 04:22:51 +0200

    Create lib/main.dart
    إضافة الملف الرئيسي main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'نقيلي - دور وقارن ووفر',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Tajawal',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
